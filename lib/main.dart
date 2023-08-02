250180f1c7e4addc3c8ac60db1c99b6f1602bfa8
Rami-Ferjani <rami.ferjani3@gmail.com>
2023-08-02 17:26:34 +0100

    first commit in my first flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,22 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        backgroundColor: Colors.blue,
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(colors: ),
+          ),
+          child: Center(
+            child: Text("Hello World"),
+          ),
         ),
       ),
     ),
   );
 }
-
-  
